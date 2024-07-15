3fedbc54912924d45a811c1db7264fe5b8c35288
Sanjoy Saha <sanjoysaha0524@gmail.com>
2024-07-15 12:09:54 +0530

    Day 1 done

--- 30 Days of JS/Day_1.js
+++ 30 Days of JS/Day_1.js
@@ -3,7 +3,7 @@ var num = 7
 console.log(num);
 
 //Task 2: Declare a variable using let, assign it a string, and log the value to the console.
-let str = "Sanjoy"
+let str = "Sanjoy Saha"
 console.log(str);
 
 //Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
