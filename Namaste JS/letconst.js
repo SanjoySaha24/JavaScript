fcc215eace3115062b6b8f7de3eef5f7f6779fdb
Sanjoy Saha <sanjoysaha0524@gmail.com>
2024-08-21 01:15:50 +0530

    closures.js

---
+++ Namaste JS/letconst.js
@@ -0,0 +1,12 @@
+// console.log(a);
+// let a=10
+// let a= 100
+
+console.log(add(4,5));
+ 
+function add(a,b){
+    return a+b 
+}
+
+var v= 34
+console.log(v);
--- Namaste JS/scope.js
+++ Namaste JS/scope.js
@@ -1,5 +1,36 @@
-function a(){
-    console.log(b);
-}
-var a = 10;
-a();
+// function a(){
+// console.log(b);
+// }
+// var b=10
+// a()
+
+// function a() {
+//     c();
+//     function c() {
+//     console.log(b); // 10
+//     }
+//     }
+//     var b = 10;
+//     a();
+
+
+//     function a() {
+//         c();
+//         function c() {
+//         var b = 100;
+//         console.log(b); 
+//         }
+//         }
+//         var b = 10;
+//         a();
+
+
+        function a() {
+            var b = 10;
+            c();
+            function c() {
+            console.log(b); 
+            }
+            }
+            a();
+            console.log(b);
